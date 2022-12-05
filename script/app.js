f8a0512e94ec7d34534bcd3caafb5aee3dd8984e
Shadab Khan <mdshadab21july@gmail.com>
2022-12-05 10:20:51 +0530

    Working on logic

--- script/app.js
+++ script/app.js
@@ -4,8 +4,9 @@ let libraryCatalog = [];
 
 const BOOK = document.querySelector("#inp-book-name");
 const AUTHOR = document.querySelector("#inp-author");
-const SUMBIT = document.querySelector("#inp-add");
+const ADD = document.querySelector("#inp-add");
 const STATUS = document.querySelector("#inp-status");
+const BOOKDISPLAY = document.querySelector(".book-wrapper");
 
 function Book(title, author, pages, publ, status) {
   this.title = title;
@@ -21,8 +22,10 @@ BOOK.addEventListener("click", () => {
 AUTHOR.addEventListener("click", () => {
   console.log("you clicked on the AUTHOR text field");
 });
-SUMBIT.addEventListener("click", () => {
+ADD.addEventListener("click", () => {
   console.log("you clicked on the SUMBIT button");
+  const WRAPPER = document.createElement("div").classList.add("book-card");
+  const lARGEHEADING = document.createElement("h1").classList.add("book-name");
 });
 STATUS.addEventListener("click", () => {
   console.log("you clicked on the STATUS drop-down options");
